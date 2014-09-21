19738e49807c33ccb51daec14a315e84b0ecb177
Callie <clarson283@gmail.com>
2014-09-21 12:13:35 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	/* add item to list */
+	$("#submit").on("click", function() {
+		var add = $("#list-item").val();
+		$("#list").append('<li><span class="added">' + add + '</span></li>');
+		$("#list-item").val("");
+		event.preventDefault();
+	})
+	$("#list-item").keydown(function(event) {
+		if(event.keyCode == 13) {
+			var add = $("#list-item").val();
+			$("#list").append('<li><span class="added">' + add + '</span></li>');
+			$("#list-item").val("");
+			event.preventDefault();
+		}
+	})
+	/*button to clear list*/
+	$("#reset").on("click", function() {
+		$("#list li").hide();
+	})
+	/*delete items*/
+	$("#list").on("click", function() {
+		$("#list").css("text-decoration","line-through");
+	})
+});
