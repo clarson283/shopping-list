6aa00184538b274744273ac46777f1479bdfbcde
Callie <clarson283@gmail.com>
2014-12-03 22:18:51 -0800

    third commit
    third commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	/* add item to list */
 	$("#submit").on("click", function() {
 		var add = $("#list-item").val();
+		$("#main").css("display", "block");
 		$("#list").append('<li><span class="added">' + add + '</span></li>');
 		$("#list-item").val("");
 		event.preventDefault();
@@ -9,6 +10,7 @@ $(document).ready(function() {
 	$("#list-item").keydown(function(event) {
 		if(event.keyCode == 13) {
 			var add = $("#list-item").val();
+			$("#main").css("display", "block");
 			$("#list").append('<li><span class="added">' + add + '</span></li>');
 			$("#list-item").val("");
 			event.preventDefault();
@@ -17,6 +19,7 @@ $(document).ready(function() {
 	/*button to clear list*/
 	$("#reset").on("click", function() {
 		$("#list li").hide();
+		$("#main").css("display", "none");
 	});
 	/*delete items*/
 	$("#list").on("click", ".added", function() {
